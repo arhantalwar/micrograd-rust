8ffcd334e7043166cf2f1812ebf2c447abda194f
arhantalwar <arhantalwar@gmail.com>
2024-02-02 00:25:10 +0530

    neuron and mlp tested

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
-use neuron::Neuron;
-
 pub mod engine;
 pub mod neuron;
 pub mod layer;
 pub mod mlp;
 
+use neuron::Neuron;
+
 fn main() {
-
-    let a = Neuron::new(2);
-    let b = Neuron::forward_pass(&a, &vec![1.0, 2.0]);
-
-    println!("--------------------- weights --------------------");
-    println!("{:?}", a.borrow().weights);
-    println!("--------------------- bias --------------------");
-    println!("{:?}", a.borrow().bias);
-    println!("--------------------- b --------------------");
-    println!("{:?}", b.borrow().data);
 
 }
 
